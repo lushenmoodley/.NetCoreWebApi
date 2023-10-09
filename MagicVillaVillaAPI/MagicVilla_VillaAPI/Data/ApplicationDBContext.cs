044b8a9c65964b79db8cced1ca98c88c5263d57c
Lushen Moodley <lushenmoodley@yahoo.com>
2023-10-09 20:34:22 +0200

    implement db

---
+++ MagicVillaVillaAPI/MagicVilla_VillaAPI/Data/ApplicationDBContext.cs
@@ -0,0 +1,13 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_VillaAPI.Data
+{
+    public class ApplicationDBContext:DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext>options) : base(options) { }
+
+
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
--- MagicVillaVillaAPI/MagicVilla_VillaAPI/Program.cs
+++ MagicVillaVillaAPI/MagicVilla_VillaAPI/Program.cs
@@ -1,14 +1,25 @@
+using MagicVilla_VillaAPI.Data;
 using MagicVilla_VillaAPI.Logging;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
 
+
+
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDBContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
+});
 
 // Add services to the container.
 builder.Services.AddControllers(option => { option.ReturnHttpNotAcceptable = true; }).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<ILogging, Logging>();
+builder.Services.AddSingleton<ILogging, LoggingV2>();
 
 
 
